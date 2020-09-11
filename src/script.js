7b9db4b60f0ad43e110c581cfaa40cc013621df9
yrauvtovich <rama7777@bk.ru>
2020-09-11 11:07:38 +0300

    Update code 11.09

--- src/script.js
+++ src/script.js
@@ -4,5 +4,13 @@ $(document).ready(function(){
         width: "400px",
         transition: time +"s",
     });
+
+    $(".point-one").addClass("delete");
+    if ($(".point-one").hasClass("delete")){
+        setTimeout ($(".point-one").addClass("show", 3000));
+    } else {
+        console.log('Error');
+    }
+
 });
 
