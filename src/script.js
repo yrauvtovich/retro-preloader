8276c62fb6637fbb6ccc3d247da21cb82960bfb7
yrauvtovich <rama7777@bk.ru>
2020-09-15 19:00:37 +0300

    Update code 15.09

--- src/script.js
+++ src/script.js
@@ -4,13 +4,46 @@ $(document).ready(function(){
         width: "400px",
         transition: time +"s",
     });
+    setTimeout(function onePoint() {
+        setInterval(function () {
+            if ($(".point-one").hasClass("delete")){
+                $(".point-one").removeClass("delete").addClass("show");
+            } else {
+                $(".point-one").addClass("delete");
+                if ($(".point-one").hasClass("show")) {
+                    $(".point-one").removeClass("show")
+                }
 
-    $(".point-one").addClass("delete");
-    if ($(".point-one").hasClass("delete")){
-        setTimeout ($(".point-one").addClass("show", 3000));
-    } else {
-        console.log('Error');
-    }
+            }
+        }, time*100);
+    }, (time-4)*80);
+    setTimeout(function twoPoint(){
+        setInterval(function () {
+            if ($(".point-two").hasClass("delete")){
+                $(".point-two").removeClass("delete").addClass("show");
+            } else {
+                $(".point-two").addClass("delete");
+                if ($(".point-two").hasClass("show")) {
+                    $(".point-two").removeClass("show")
+                }
+
+            }
+        }, time*100);
+    }, (time-3)*80);
+    setTimeout(function threePoint() {
+        setInterval(function () {
+            if ($(".point-three").hasClass("delete")) {
+                $(".point-three").removeClass("delete").addClass("show");
+            } else {
+                $(".point-three").addClass("delete");
+                if ($(".point-three").hasClass("show")) {
+                    $(".point-three").removeClass("show")
+                }
+
+            }
+        }, time * 100);
+    },
+        (time-2)*80);
 
 });
 
