ab7a69122c2f0afc34c976c1aa773661a97faa97
yrauvtovich <rama7777@bk.ru>
2020-10-08 20:40:13 +0300

    Update code 08.10

--- src/script.js
+++ src/script.js
@@ -1,55 +1,50 @@
 $(document).ready(function(){
 
 //Setting the download time in seconds
-    let time = 5;
+  let time = 5;
 
 //Download options
-    $(".preloader-download").animate({
-        width: "400px",
-        transition: time +"s",
-    });
+  $(".preloader-download").animate({
+    width: "400px",
+    transition: time +"s",
+  });
 
 //Blinking points
-    setTimeout(function onePoint() {
-        setInterval(function () {
-            if ($(".point-one").hasClass("point-hide")){
-                $(".point-one").removeClass("point-hide").addClass("point-show");
-            } else {
-                $(".point-one").addClass("point-hide");
-                if ($(".point-one").hasClass("point-show")) {
-                    $(".point-one").removeClass("point-show")
-                }
-
-            }
-        }, time*100);
-    }, (time-4)*80);
-    setTimeout(function twoPoint(){
-        setInterval(function () {
-            if ($(".point-two").hasClass("point-hide")){
-                $(".point-two").removeClass("point-hide").addClass("point-show");
-            } else {
-                $(".point-two").addClass("point-hide");
-                if ($(".point-two").hasClass("point-show")) {
-                    $(".point-two").removeClass("point-show")
-                }
-
-            }
-        }, time*100);
-    }, (time-3)*80);
-    setTimeout(function threePoint() {
-        setInterval(function () {
-            if ($(".point-three").hasClass("point-hide")) {
-                $(".point-three").removeClass("point-hide").addClass("point-show");
-            } else {
-                $(".point-three").addClass("point-hide");
-                if ($(".point-three").hasClass("point-show")) {
-                    $(".point-three").removeClass("point-show")
-                }
-
-            }
-        }, time * 100);
-    },
-        (time-2)*80);
-
+  setTimeout(function onePoint() {
+    setInterval(function () {
+      if ($(".point-one").hasClass("point-hide")){
+        $(".point-one").removeClass("point-hide").addClass("point-show");
+      } else {
+        $(".point-one").addClass("point-hide");
+        if ($(".point-one").hasClass("point-show")) {
+                    $(".point-one").removeClass("point-show");
+        }
+      }
+    }, time*100);
+  }, (time-4)*80);
+  setTimeout(function twoPoint(){
+    setInterval(function () {
+      if ($(".point-two").hasClass("point-hide")){
+        $(".point-two").removeClass("point-hide").addClass("point-show");
+      } else {
+        $(".point-two").addClass("point-hide");
+        if ($(".point-two").hasClass("point-show")) {
+          $(".point-two").removeClass("point-show")
+        }
+      }
+    }, time*100);
+  }, (time-3)*80);
+  setTimeout(function threePoint() {
+    setInterval(function () {
+      if ($(".point-three").hasClass("point-hide")) {
+        $(".point-three").removeClass("point-hide").addClass("point-show");
+      } else {
+        $(".point-three").addClass("point-hide");
+        if ($(".point-three").hasClass("point-show")) {
+          $(".point-three").removeClass("point-show")
+        }
+      }
+    }, time * 100);
+  }, (time-2)*80);
 });
 
